d75f15105817508982bf1d7d653007a08b0ef89d
deepakv30 <deeptech30@gmail.com>
2019-04-09 22:39:10 +0530

    this is just for test

---
+++ test.js
@@ -0,0 +1,10 @@
+var x = document.getElementsByClassName('button-secondary-medium');
+for(var i= 0; i < 1; i++){
+    x[i].click();
+} 
+<button class="invitation-card__action-btn button-secondary-medium " data-control-name="accept" data-ember-action="" data-ember-action-50="50">
+            <span aria-hidden="true">Accept</span>
+            <span class="visually-hidden">
+              Accept Chandra Mohan Singh Negi ( CEng, MIE)’s invitation
+            </span>
+        </button>
